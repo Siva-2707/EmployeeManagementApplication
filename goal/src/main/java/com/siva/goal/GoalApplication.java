4c9a8c82a5cf5ed843150852ce72bec5ccc58415
Siva-2707 <b.sivarajesh20@gmail.com>
2023-01-16 11:33:10 +0530

    Component scan removed in Main class

--- goal/src/main/java/com/siva/goal/GoalApplication.java
+++ goal/src/main/java/com/siva/goal/GoalApplication.java
@@ -2,10 +2,8 @@ package com.siva.goal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(scanBasePackages = "com.siva.goal.repository")
-@ComponentScan("com.siva.goal")
 public class GoalApplication {
 
 	// private final EmployeeService employeeService;
